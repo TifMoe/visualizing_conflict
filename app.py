909394225118ef7185f2a7d1f2a094b09ffb080e
Tiffany Moeller <tmoellerart@gmail.com>
2018-08-31 14:50:36 -0500

    added timeline, color scheme

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
+from datetime import datetime
 
 app = dash.Dash()
 
-df = pd.read_csv('data/world_conflict.csv', usecols=['longitude', 'latitude', 'region',
+df = pd.read_csv('data/world_conflict.csv', usecols=['longitude', 'latitude', 'region', 'admin1', 'location',
                                                      'event_type', 'fatalities', 'event_date'])
+
+df = df.loc[:20000].copy()
+
+df['event_type'] = ['Battle' if e[:6] == 'Battle' else e
+                    for e in df['event_type']]
+
+df['event_date'] = [datetime.strptime(d, "%d-%b-%y") for d in df['event_date']]
+df_days = df.groupby(['event_date', 'event_type'], as_index=False).agg({'fatalities': ['count', 'sum']})
+
 margin = 5
 
 min_x = min(df['longitude']) - margin
@@ -19,8 +29,56 @@ max_y = max(df['latitude']) + margin
 colors = {
     'background': '#F7F7F7',
     'text': '#37575B',
-    'markers': '#ABC0C1'
+    'markers': '#ABC0C1',
+    'event_types': {'Violence against civilians': '#810f7c',
+                    'Riots/Protests': '#e0ecf4',
+                    'Remote violence': '#8c96c6',
+                    'Battle': '#8856a7'}
 }
+
+
+def generate_timeline(df):
+    print(df.event_type.unique())
+    return dcc.Graph(
+                    id='conflict_timeline',
+                    figure={
+                        'data': [
+                            go.Scatter(
+                                x=df[df['event_type']==i]['event_date'],
+                                y=df[df['event_type']==i]['fatalities']['sum'],
+                                line=dict(color=colors['event_types'][i])
+                            ) for i in df.event_type.unique()
+                        ],
+                        'layout': go.Layout(
+                            plot_bgcolor=colors['background'],
+                            paper_bgcolor=colors['background'],
+                            height=350,
+                            title='Fatalities per day',
+                            font={'color': colors['text']},
+                            margin={'l': 30, 'b': 30, 't': 30, 'r': 10},
+                            showlegend=False,
+                            hovermode='closest',
+                            xaxis=dict(
+                                    rangeselector=dict(
+                                        buttons=list([
+                                            dict(count=1,
+                                                 label='1m',
+                                                 step='month',
+                                                 stepmode='backward'),
+                                            dict(count=6,
+                                                 label='6m',
+                                                 step='month',
+                                                 stepmode='backward'),
+                                            dict(step='all')
+                                        ])
+                                    ),
+                                    rangeslider=dict(
+                                        visible=True
+                                    ),
+                                    type='date'
+                                ))
+                    }
+                )
 
 
 def generate_map(df, colors, title, min_x, max_x, min_y, max_y):
@@ -35,7 +93,8 @@ def generate_map(df, colors, title, min_x, max_x, min_y, max_y):
                                         'size': 10,
                                         'line': {'width': 0.3,
                                                  'color': colors['text']},
-                                        'color': colors['markers'],
+                                        'color': [colors['event_types'][e]
+                                                  for e in df[df['event_type'] == i]['event_type']],
                                         'opacity':.5
                                     },
                                     name=i
@@ -76,7 +135,7 @@ def generate_map(df, colors, title, min_x, max_x, min_y, max_y):
 def generate_hist(df):
     return dcc.Graph(
                     id='conflict_type_hist',
-                    clickData={'points': [{'text': 'Remote violence'}]},
+                    clickData={'points': [{'text': list(colors['event_types'].keys())}]},
                     figure={
                         'data': [
                             go.Histogram(
@@ -84,7 +143,8 @@ def generate_hist(df):
                                 text=df[df['event_type'] == i]['event_type'],
                                 name=i,
                                 marker=dict(
-                                            color=colors['markers'],
+                                            color=[colors['event_types'][e]
+                                                   for e in df[df['event_type'] == i]['event_type']],
                                             line={'width': 0.5,
                                                   'color': colors['text']},
                                             opacity=.7
@@ -97,7 +157,7 @@ def generate_hist(df):
                             autosize=True,
                             title='Conflict Event Types',
                             font={'color': colors['text']},
-                            margin={'l': 300, 'b': 40, 't': 30, 'r': 10},
+                            margin={'l': 200, 'b': 40, 't': 30, 'r': 10},
                             showlegend=False,
                             hovermode='closest'
                         )
@@ -110,45 +170,65 @@ app.layout = html.Div(
             html.Div(className='container-fluid',
                      children=[html.Br(),
                                html.H2('Visualizing Conflict', style={'color': colors['text']}),
-                               html.Div(
-                                   dcc.Dropdown(id='countries',
-                                                multi=True,
-                                                value=[''],
-                                                placeholder='Select Countries',
-                                                options=[{'label': c, 'value': c}
-                                                         for c in sorted(df['region'].unique())]
-                                                ),
-                                   style={'width': '25%', 'margin-left': '5%', 'background-color': 'white',
-                                          'font-size': '12px', 'color': colors['text']}),
+
+                               html.Div(className='row',
+                                        children=[
+                                           html.Div(
+                                               dcc.Dropdown(id='countries',
+                                                            multi=True,
+                                                            value=[''],
+                                                            placeholder='Select Countries',
+                                                            options=[{'label': c, 'value': c}
+                                                                     for c in sorted(df['region'].unique())]
+                                                            ),
+                                               className='col s3 m3 l3 center-align',
+                                               style={'font-size': '12px',
+                                                      'color': colors['text']}),
+
+                                           html.Br(),
+                                           html.Br(),
+
+                                           html.Div(id='conflict_timeline_div',
+                                                    className='col s12',
+                                                    children=generate_timeline(df_days))
+                                    ]),
+
                                html.Br(),
-                               html.Br()
+
+                               html.Div(className='row', children=[
+                                   html.Div(id='conflict_map_div', children='', className='col s12 m8 l8'),
+                                   html.Div(id='conflict_type_hist_div',
+                                            className='col s12 m4 l4',
+                                            children=generate_hist(df))]),
+
                                ],
-                     style={'width': '90%', 'margin-left': '5%', 'margin-right': '5%'}),
+                     style={'width': '90%', 'margin-left': '5%', 'margin-right': '5%'}
+                     )
 
-            html.Div(className='row', children=[
-                 html.Div(id='conflict_map_div', children='', className='col s12 m6 l6'),
-                 html.Div(id='conflict_type_hist_div',
-                          className='col s12 m6 l6',
-                          children=generate_hist(df))]),
+            ],
+    style={'backgroundColor': colors['background'], 'margin-top': '-30px', 'height': '2000px'})
 
-                ],
-    style={'backgroundColor': colors['background'], 'margin-top':'-30px', 'height':'2000px'}
-    )
 
 
 @app.callback(
     dash.dependencies.Output('conflict_map_div', 'children'),
     [dash.dependencies.Input('conflict_type_hist', 'clickData')])
 def update_map(clickData):
-    try:
-        conflict_type = [clickData["points"][0]['text']]
-        mapping_events = ", ".join(conflict_type)
-    except TypeError:
-        conflict_type = list(df.event_type.unique())
-        mapping_events = 'All Events'
+
+    conflict_type = clickData["points"][0]['text']
+
+    if type(conflict_type) == list:
+        if len(conflict_type) == df['event_type'].nunique():
+            mapping_events = 'All Events'
+        else:
+            mapping_events = ", ".join(conflict_type)
+        mask = [True if i in set(conflict_type) else False for i in list(df['event_type'])]
+
+    else:
+        mapping_events = conflict_type
+        mask = [True if i == conflict_type else False for i in list(df['event_type'])]
 
     # Mask for selected event types
-    mask = [True if i in set(conflict_type) else False for i in list(df['event_type'])]
     dff = df[mask]
 
     title = 'Mapping {}'.format(mapping_events)
